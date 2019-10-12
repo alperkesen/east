07b3819af2f3a88630555487b1be55536830dfd3
alperkesen <kesen.alper@gmail.com>
2019-10-12 12:50:26 +0300

    Get feature maps from vgg16

--- model.py
+++ model.py
@@ -11,4 +11,19 @@ class EAST(nn.Module):
         if extractor == "vgg16":
             self.extractor = models.vgg16(pretrained=True)
 
+            for param in self.extractor.parameters():
+                param.requires_grad = False
+
+    def forward(self, x):
+        self.pool2 = self.extractor.features[0:10]
+        self.pool3 = self.extractor.features[10:17]
+        self.pool4 = self.extractor.features[17:24]
+        self.pool5 = self.extractor.features[24:]
+
+        f4 = self.pool2(x)
+        f3 = self.pool3(f4)
+        f2 = self.pool4(f3)
+        f1 = self.pool5(f2)
+
+        self.feature_maps = [f1, f2, f3, f4]
         
